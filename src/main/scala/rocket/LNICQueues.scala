cd929b65874c4d84d9b7bcabe93be78fc6d34e40
Alex Mallery <amallery@gmail.com>
2020-07-17 09:53:25 +0000

    When a packet arrives in a cores rx queue while that core is busy servicing a timer interrupt, it is possible for the core to have already committed to scheduling the lru context and to miss the lnic interrupt. If another message arrives for the LRU-chosen context while it is already running but while the NIC does not want it to be running, the NIC will then notice that that context is already running, but at a different priority than intended (because LRU-chosen contexts are given lower priority).

--- src/main/scala/rocket/LNICQueues.scala
+++ src/main/scala/rocket/LNICQueues.scala
@@ -470,7 +470,7 @@ class LNICRxQueues(implicit p: Parameters) extends Module {
   val reg_reg_check = Reg(next = reg_check) // wait for msg_done/idle to update reg_top_context and reg_top_priority
   val reg_priority_lowered = Reg(next = priority_lowered)
   when (reg_reg_do_enq && (reg_top_priority < io.cur_priority)) {
-    assert(io.cur_context =/= reg_top_context, "Priorities don't match, but context IDs do?")
+    // assert(io.cur_context =/= reg_top_context, "Priorities don't match, but context IDs do?")
     io.interrupt := true.B
   } .elsewhen (reg_reg_check && (reg_top_context =/= io.cur_context)) {
     io.interrupt := true.B
